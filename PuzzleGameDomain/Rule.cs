27b2f3a312c4d1e7e395a6087978120405aa3f96
JBink <jbink@sogyo.nl>
2020-06-25 15:45:31 +0200

    created standard row rule

--- PuzzleGameDomain/Rule.cs
+++ PuzzleGameDomain/Rule.cs
@@ -3,7 +3,7 @@
     public abstract class Rule
     {
         public virtual int Id { get; set; }
-        public abstract bool SquareBreaksRule(Square[][] board, int rowIndex, int columnIndex);
+        public abstract bool CheckSquareBreaksRule(Square[][] board, int squareRowIndex, int squareColumnIndex);
 
         public abstract Square[][] PropagateSquareInfluence(Square[][] board, int rowIndex, int columnIndex);
 
--- PuzzleGameDomain/Rules/StandardRowRule.cs
+++ PuzzleGameDomain/Rules/StandardRowRule.cs
@@ -8,9 +8,19 @@
             throw new System.NotImplementedException();
         }
 
-        public override bool SquareBreaksRule(Square[][] board, int rowIndex, int columnIndex)
+        public override bool CheckSquareBreaksRule(Square[][] board, int squareRowIndex, int squareColumnIndex)
         {
-            throw new System.NotImplementedException();
+            var breaksRule = false;
+            var squareValue = board[squareRowIndex][squareColumnIndex].Value;
+            if (squareValue != 0)
+            {
+                for (int columnIndex = 0; columnIndex < board[0].Length; columnIndex++)
+                {
+                    if (columnIndex != squareColumnIndex && board[squareRowIndex][columnIndex].Value == squareValue) breaksRule = true;
+                }
+            }
+
+            return breaksRule;
         }
     }
 }
